937d4c32d8d5cb3426320b5247d0016ecd173962
Saurabh Pathak <pathaks@tcd.ie>
2017-03-05 18:08:29 +0000

    fixing grid draw functionality

--- Assets/Scripts/Tile Layout/TilingSystem.cs
+++ Assets/Scripts/Tile Layout/TilingSystem.cs
@@ -31,75 +31,48 @@ public class TilingSystem : MonoBehaviour {
 		}
 	}
 
+	private TileSprite getTile(Tiles tile) {
+		foreach(TileSprite tileSprite in TileSprites) {
+			if (tileSprite.tileType == tile)
+				return tileSprite;
+		}
+		return TileSprites [0];
+	}
+
 	//set the tiles of the map to what is specified in TileSprites
 	private void SetTiles() {
+		_map [0, 1] = new TileSprite (0, Tiles.Mountains);
+		_map [0, 4] = new TileSprite (0, Tiles.Mountains);
+		_map [1, 2] = new TileSprite (0, Tiles.Mountains);
+		_map [1, 1] = new TileSprite (0, Tiles.Mountains);
+		_map [2, 5] = new TileSprite (0, Tiles.Mountains);
+		_map [2, 3] = new TileSprite (0, Tiles.Mountains);
+		_map [3, 3] = new TileSprite (0, Tiles.Mountains);
+		_map [3, 5] = new TileSprite (0, Tiles.Mountains);
+		_map [4, 1] = new TileSprite (0, Tiles.Mountains);
+		_map [4, 4] = new TileSprite (0, Tiles.Mountains);
+	}
 
-		string[] gridData = new string[] {
-			"0101100010101",
-			"0000010101010",
-			"0001000101001",
-			"0100110011000",
-			"1000100000010",
-			"0101010100101"
-		};
+	private void PlaceTile(TileSprite tileSprite, int x, int y, Vector3 gridStart) {
+		GameObject newTile = Instantiate (getTile (tileSprite.tileType).tilePrefab);
+		newTile.transform.position = new Vector3 (gridStart.x + (getTileSize() * x), gridStart.y - (getTileSize() * y), 0);
+	}
 
+	private void AddTilesToMap() {
 		Vector3 gridStart = Camera.main.ScreenToWorldPoint (new Vector3 (0, Screen.height));
 
 		for (int y = 0; y < MapSize.y; y++) {
-			char[] rowTiles = gridData [y].ToCharArray ();
 			for (int x = 0; x < MapSize.x; x++) {
-				PlaceTile (rowTiles[x].ToString(), x, y, gridStart);
+				PlaceTile (_map[x, y], x, y, gridStart);
 			}
 		}
 	}
 
-	private void PlaceTile(string tileType, int x, int y, Vector3 gridStart) {
-		GameObject newTile = Instantiate (TileSprites[int.Parse(tileType)].tilePrefab);
-		newTile.transform.position = new Vector3 (gridStart.x + (getTileSize() * x), gridStart.y - (getTileSize() * y), 0);
-	}
-
-//	private void AddTilesToMap() {
-//
-//		_tileContainer = LeanPool.Spawn (TileContainerPrefab);
-//
-//		var tileSize = 2f;
-//		var viewOffsetX = ViewPortSize.x;
-//		var viewOffsetY = ViewPortSize.y;
-//
-//		for (var y = -viewOffsetY; y < viewOffsetY; y++) {
-//			for (var x = -viewOffsetX; x < viewOffsetX; x++) {
-//				var tX = x * tileSize;
-//				var tY = y * tileSize;
-//
-//				var iX = x + CurrentPosition.x;
-//				var iY = y + CurrentPosition.y;
-//
-//				if (iX < 0)
-//					continue;
-//				if (iY < 0)
-//					continue;
-//				if (iX > MapSize.x - 2)
-//					continue;
-//				if (iY > MapSize.y - 2)
-//					continue;
-//
-//				var tile = LeanPool.Spawn (TilePrefab);
-//				tile.transform.position = new Vector3 (tX, tY, 0);
-//				tile.transform.SetParent (_tileContainer.transform);
-//				
-//				//set an image for the tile - do this when you create your tile prefabs (for shack, mountains, ...)
-//				var renderer = tile.GetComponent<SpriteRenderer> ();
-//				/renderer.sprite = _map [(int)x + (int)CurrentPosition.x, (int)y + (int)CurrentPosition.y].TileImage;
-//				
-//				_tiles.Add (tile);
-//			}
-//		}
-//	}
-
 	public void Start() {
 		_map = new TileSprite[(int)MapSize.x, (int)MapSize.y];
+		DefaultTiles ();
 		SetTiles ();
-		//AddTilesToMap ();
+		AddTilesToMap ();
 	}
 
 }
