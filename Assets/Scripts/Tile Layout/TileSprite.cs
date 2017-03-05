47823a70169303e0e1592ac7624349f488db0c06
Saurabh Pathak <pathaks@tcd.ie>
2017-03-05 13:10:56 +0000

    creating the tile grid

---
+++ Assets/Scripts/Tile Layout/TileSprite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[Serializable]
+public class TileSprite : MonoBehaviour {
+
+	//define what atributes a tile has
+	// ...
+
+	public TileSprite() {
+		
+		// initialize a default tile
+		// ...
+
+	}
+}
