d0fa0b21110726e3dd802148706b61a423818d85
Saurabh Pathak <pathaks@tcd.ie>
2017-03-30 19:47:01 +0100

    adding travel to target class to make the agent move

---
+++ Assets/Scripts/Miner States/MinerTravelToTarget.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class MinerTravelToTarget : TravelToTarget<Miner>
+{
+    //public MinerTravelToTarget(Location target, State<Miner> state)
+    //{
+    //    targetPosition = LocationProperties.LocationCoords[(int)target];
+    //    targetState = state;
+    //}
+
+    public MinerTravelToTarget(State<Miner> state)
+    {
+        targetPosition = LocationProperties.LocationCoords[(int)target];
+        targetState = state;
+    }
+
+    public override void Enter(Miner miner)
+    {
+        //path = pathFinder.FindPath(miner.CurrentPosition, targetPosition);
+
+        Debug.Log("Walkin' to " + LocationProperties.ToString(LocationProperties.GetLocation(targetPosition)) + ".");
+    }
+
+    public override void Execute(Miner miner)
+    {
+        //if (path.Count > 0)
+        //{
+        //    for (int i = 0; i < path.Count; ++i)
+        //    {
+        //        path[i].TintColor = Color.Blue;
+        //        path[i].TintAlpha = 0.5f;
+        //    }
+
+        //    miner.CurrentPosition = path[0].Position;
+        //    path.RemoveAt(0);
+        //}
+        //else
+        //{
+        //    miner.CurrentPosition = targetPosition;
+
+        //    State<Miner> previousState = miner.StateMachine.PreviousState;
+        //    miner.StateMachine.ChangeState(targetState);
+        //    miner.StateMachine.PreviousState = previousState;
+        //}
+    }
+
+    public override void Exit(Miner miner)
+    {
+        //path.Clear();
+    }
+
+    public override bool OnMesssage(Miner agent, Telegram telegram)
+    {
+        return false;
+    }
+
+    public override bool OnSenseEvent(Miner agent, Sense sense)
+    {
+        return false;
+    }
+}
--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -1,4 +1,6 @@
-﻿abstract public class State <T> {
+﻿using System.Collections.Generic;
+using UnityEngine;
+abstract public class State <T> {
 
 	abstract public void Enter (T agent);
 	abstract public void Execute (T agent);
@@ -9,9 +11,9 @@
     abstract public class TravelToTarget<T> : State<T>
     {
         protected static AStar pathFinder = new AStar();
-        //protected List<Tile> path;
+        protected List<Node> path;
 
-        //protected Vector2 targetPosition;
+        protected Vector2 targetPosition;
         protected State<T> targetState;
     }
 }
